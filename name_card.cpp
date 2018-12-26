c79c0bba6198a80322c8ee41358b74e1e4676add
237K <237ref@gmail.com>
2018-12-26 10:29:21 +0900

    복사생성자와 얕은복사, 깊은복사에 대해 공부하였습니다. 얕은복사로 인해 발생할 수 있는 오류에 대해 이해하였고, 깊은복사를 위한 복사생성자를 명시하여 적용하였습니다.

--- name_card.cpp
+++ name_card.cpp
@@ -5,16 +5,15 @@
 //  Created by 형성모 on 2018. 12. 25..
 //
 //  동적할당을 이용하여 명함 관리 프로그램을 개발하였습니다.
-//
+//  ver2. 복사생성자와 얕은복사, 깊은복사에 대해 배웠으며, 얕은복사로 인한 에러 상황을 이해하였습니다.
+//  ver2. 복사생성자를 정의함으로써 깊은복사를 가능하도록 하여 소스의 안정성을 높였습니다.
 
 #include <iostream>
 #include <cstring>
-using namespace std;
-/*
+
 using std::cout;
 using std::cin;
 using std::endl;
-*/
  
 namespace COMP_POS
 {
@@ -47,34 +46,39 @@ private:
 public:
     NameCard(char *name1, char *company1, char *mobile1, int position);
     ~NameCard();
+    NameCard(const NameCard &copy);
     void ShowNameCard() const;
 };
 
 NameCard::NameCard(char *name1, char *company1, char *mobile1, int position1)
 {
-    //int name_len = strlen(name1) + 1;
     name = new char[strlen(name1)+1];
     strcpy(name, name1);
-    
-    //int company_len = strlen(company1) + 1;
+
     company = new char[strlen(company1)+1];
     strcpy(company, company1);
     
-    //int mobile_len = strlen(mobile1) + 1;
     mobile = new char[strlen(mobile1)+1];
     strcpy(mobile, mobile1);
     
     position = position1;
 }
 
+NameCard::NameCard(const NameCard &copy) :position(copy.position)
+{
+    name = new char[strlen(copy.name)+1];
+    strcpy(name, copy.name);
+    company = new char[strlen(copy.company)+1];
+    strcpy(company, copy.company);
+    mobile = new char[strlen(copy.mobile)+1];
+    strcpy(mobile, copy.mobile);
+}
+
 NameCard::~NameCard()
 {
     delete []name;
-    cout<<"name에 할당된 동적공간을 해제하였습니다."<<endl;
     delete []company;
-    cout<<"company에 할당된 동적공간을 해제하였습니다."<<endl;
     delete []mobile;
-    cout<<"mobile에 할당된 동적공간을 해제하였습니다."<<endl;
 }
 
 void NameCard::ShowNameCard() const
@@ -97,6 +101,14 @@ int main(void)
     manCLERK.ShowNameCard();
     manSENIOR.ShowNameCard();
     manASSIST.ShowNameCard();
+    
+    NameCard copy1 = manCLERK; //NameCard copy1(manCLERK) 의 형태로 묵시적 형변환이 발생합니다.
+    NameCard copy2 = manSENIOR;
+    
+    copy1.ShowNameCard();
+    copy2.ShowNameCard();
+    
+    cout<<"깊은복사가 제대로 이루어졌는지 확인합니다."<<endl;
     
     return 0;
 }
