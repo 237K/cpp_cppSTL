c0bb7a9da4d17645efead7f08454874a73009677
237K <237ref@gmail.com>
2019-01-12 17:29:33 +0900

    [STL 알고리즘] <1.원소를 수정하지 않는 알고리즘> 8)lexicographical_compare, 9)max, min, 10)max_element, min_element

--- STL_Algorithm_Nonmodifying.cpp
+++ STL_Algorithm_Nonmodifying.cpp
@@ -13,6 +13,9 @@
 //          5) find_end(v1.begin(), v1.end(), v2.begin(), v2.end(), (func)) : v1에 v2와 같은 순차열이 있는지 확인, 마지막 순차열의 첫 번째 원소반환
 //          6) find_first_of(v.begin(), v.end(), v2.begin(), v2.end(), (func)) : find_end처럼 구간 검색, 모든 원소 중 같은 원소가 하나라도 발견되면 발견된 첫 번쩨 원소의 반복자 반환
 //          7) for_each(v.begin(), v.end(), func()) : 구간의 모든 원소에 대해 func을 적용. 원소를 수정하는 알고리즘에도 포함
+//          8) lexicographical_compare(v.begin(), v.end(), v2.begin(), v2.end(), (func)) : 사전편찬식으로 비교
+//          9) max, min(v.begin(), v.end(), (func))
+//          10) max_element, min_element(v.begin(), v.end(), (func))
 //
 
 #include <iostream>
@@ -53,6 +56,37 @@ public:
         cout<<t<<ch;
     }
 };
+
+class Point
+{
+private:
+    int xpos, ypos;
+public:
+    explicit Point(int x=0, int y=0) :xpos(x), ypos(y) {}
+    int GetX() const { return xpos; }
+    int GetY() const { return ypos; }
+    void Print() const { cout<<"("<<xpos<<", "<<ypos<<")"<<endl; }
+    friend ostream& operator<<(ostream& os, const Point& pnt);
+};
+ostream& operator<<(ostream& os, const Point& pnt)
+{
+    os<<"("<<pnt.xpos<<", "<<pnt.ypos<<")"<<endl;
+    return os;
+}
+bool Xcompare(const Point& pnt1, const Point& pnt2)
+{
+    return pnt1.GetX() < pnt2.GetX() ? true : false;
+}
+bool Ycompare(const Point& pnt1, const Point& pnt2)
+{
+    return pnt1.GetY() < pnt2.GetY() ? true : false;
+}
+bool Pcompare(const Point& pnt1, const Point& pnt2)
+{
+    if(pnt1.GetX() < pnt2.GetX()) return true;
+    else if(pnt1.GetX() == pnt2.GetX() && pnt1.GetY() <= pnt2.GetY()) return true;
+    else return false;
+}
 
 int main(void)
 {
@@ -172,6 +206,94 @@ int main(void)
     cout<<endl;
     for_each(v2.begin(), v2.end(), PrintVector<int>('\n'));
     cout<<endl<<endl;
+    
+    //lexicographical_compare
+    cout<<"lexicographical_compare"<<endl;
+    vector<int> v3;
+    v3.push_back(10);
+    v3.push_back(20);
+    v3.push_back(50);
+    for(iter = v.begin(); iter != v.end(); ++iter)
+    {
+        cout<<*iter<<' ';
+    }
+    cout<<endl;
+    for(iter = v2.begin(); iter != v2.end(); ++iter)
+    {
+        cout<<*iter<<' ';
+    }
+    cout<<endl;
+    for(iter = v3.begin(); iter != v3.end(); ++iter)
+    {
+        cout<<*iter<<' ';
+    }
+    cout<<endl;
+    bool bl2 = lexicographical_compare(v.begin(), v.end(), v3.begin(), v3.end());
+    bool bl3 = lexicographical_compare(v.begin(), v.end(), v2.begin(), v2.end());
+    bool bl4 = lexicographical_compare(v.begin(), v.end(), v3.begin(), v3.end(), greater<int>());
+    bool bl5 = lexicographical_compare(v.begin(), v.end(), v2.begin(), v2.end(), greater<int>());
+    
+    if(bl2) cout<<"v < v3"<<endl;
+    else cout<<"v >= v3"<<endl;
+    
+    if(bl3) cout<<"v < v2"<<endl;
+    else cout<<"v >= v2"<<endl;
+    cout<<endl;
+    
+    //lexicographical_compare, greater
+    cout<<"lexicographical_compare을 greater로 비교"<<endl;
+    if(bl4) cout<<"v > v3 ? True"<<endl;
+    else cout<<"v > v3 ? False"<<endl;
+    
+    if(bl5) cout<<"v > v2 ? True"<<endl;
+    else cout<<"v > v2 ? False"<<endl;
+    cout<<endl;
+    
+    //max, min
+    cout<<"max, min"<<endl;
+    Point pt1(5, 8), pt2(3, 9), pt3;
+    cout<<"Point1 : "<<pt1;
+    cout<<"Point2 : "<<pt2;
+    pt3 = max(pt1, pt2, Xcompare);
+    cout<<"max(Point1, Point2, X) : "<<pt3;
+    pt3 = max(pt1, pt2, Ycompare);
+    cout<<"max(Point1, Point2, Y) : "<<pt3;
+    pt3 = min(pt1, pt2, Xcompare);
+    cout<<"min(Point1, Point2, X) : "<<pt3;
+    pt3 = min(pt1, pt2, Ycompare);
+    cout<<"min(Point1, Point2, Y) : "<<pt3;
+    cout<<endl;
+    
+    //max_element, min_element
+    vector<int>::iterator max_iter;
+    vector<int>::iterator min_iter;
+    max_iter = max_element(v.begin(), v.end());
+    min_iter = min_element(v.begin(), v.end());
+    cout<<"max_iter : "<<*max_iter<<endl;
+    cout<<"min_iter : "<<*min_iter<<endl;
+    cout<<endl;
+    
+    //max_element, min_element (func)
+    vector<Point> v4;
+    v4.push_back(Point(3,2));
+    v4.push_back(Point(2,5));
+    v4.push_back(Point(1,5));
+    v4.push_back(Point(3,3));
+    v4.push_back(Point(3,2));
+    vector<Point>::iterator point_iter;
+    for(point_iter = v4.begin(); point_iter != v4.end(); ++point_iter)
+    {
+        cout<<*point_iter;
+    }
+    cout<<endl;
+    vector<Point>::iterator point_max_iter;
+    vector<Point>::iterator point_min_iter;
+    
+    point_max_iter = max_element(v4.begin(), v4.end(), Pcompare);
+    point_min_iter = min_element(v4.begin(), v4.end(), Pcompare);
+    cout<<"max_element : "<<*point_max_iter;
+    cout<<"min_element : "<<*point_min_iter;
+    cout<<endl;
     
     return 0;
 }
