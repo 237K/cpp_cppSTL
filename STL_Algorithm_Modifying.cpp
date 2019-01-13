51ccabe877768069e69e748faf0e2c41190b184c
237K <237ref@gmail.com>
2019-01-13 20:47:49 +0900

    [STL 알고리즘] <2. 원소를 수정하는 알고리즘> 2)copy_backward, 3)fill, 4)fill_n

--- STL_Algorithm_Modifying.cpp
+++ STL_Algorithm_Modifying.cpp
@@ -6,7 +6,10 @@
 //
 //  STL 알고리즘 #include <algorithm>
 //      2. 원소를 수정하는 알고리즘
-//          1) copy(v1.begin(), v1.end(), v2.begin()) : v1 순차열을 v2에 복사
+//          1) copy(v1.begin(), v1.end(), v2.begin()) : v1 순차열을 v2에 복사(덮어쓰기). v2는 v1보다 커야함.
+//          2) copy_backward(v1.begin(), v1.end(), v2.end()) : v1순차열을 v2 끝에서부터 복사해서 붙여넣기(덮어씀)
+//          3) fill(v1.begin(), v1.end(), x) : v1을 x로 채움
+//          4) fill_n(v1.begin(), n, x) : v1 [begin, begin+n)을 x로 채움
 //
 
 #include <iostream>
@@ -35,13 +38,38 @@ int main(void)
     vector<int> v2;
     v2.resize(5);
     
-    vector<int>::iterator copy_end_iter;
-    
-    copy_end_iter = copy(v1.begin(), v1.end(), v2.begin());
+    //copy
+    cout<<"1) copy"<<endl;
+    vector<int>::iterator copy_iter;
+    copy_iter = copy(v1.begin(), v1.end(), v2.begin());
     
     cout<<"v1 : "; Print(v1);
-    cout<<"copy_end_iter : "<<*(copy_end_iter-1)<<endl;
+    cout<<"copy_iter : "<<*(copy_iter-1)<<endl;
     cout<<"v2 : "; Print(v2);
+    cout<<endl;
+    
+    //copy_backward
+    cout<<"2) copy_backward"<<endl;
+    v2.clear();
+    v2.resize(10);
+    copy_iter = copy_backward(v1.begin(), v1.end(), v2.end());
+    
+    cout<<"v1 : "; Print(v1);
+    cout<<"copy_backward_iter : "<<*copy_iter<<endl;
+    cout<<"v2 : "; Print(v2);
+    cout<<endl;
+    
+    //fill
+    cout<<"3) fill"<<endl;
+    fill(v1.begin(), v1.end(), 0);
+    cout<<"v1을 0으로 채움 : "; Print(v1);
+    cout<<endl;
+    
+    //fill_n
+    cout<<"4) fill_n"<<endl;
+    fill_n(v2.begin(), 5, 55);
+    cout<<"v2의 시작부터 5번째까지를 55로 채움 : "; Print(v2);
+    cout<<endl;
     
     return 0;
 }
