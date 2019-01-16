5e6b40ce00684928b09763513c8d66aad333d724
237K <237ref@gmail.com>
2019-01-16 19:59:31 +0900

    [STL 알고리즘] <5.정렬 알고리즘> 3)sort, 4)stable_sort, 5)partial_sort, 6)partial_sort_copy

--- STL_Algorithm_Sorting.cpp
+++ STL_Algorithm_Sorting.cpp
@@ -12,6 +12,10 @@
 //              c) pop_heap(b, e, (f)) : 루트노드(가장 큰 원소) 제거. 루트노드와 마지막노드를 교환한 후 힙정렬 실행됨
 //              d) sort_heap(b, e, (f)) : 힙정렬
 //          2) nth_element(b, middle_iter, e) : middle_iter-b 만큼의 원소를 선별(사전순)하여 [b, middle_iter)에 저장
+//          3) sort(b, e) : 퀵소트
+//          4) stable_sort(b, e) : 머지정렬
+//          5) partial_sort(b, e) : 힙정렬
+//          6) partial_sort_copy(v1.begin, v1.end, v2.begin, v2.end) : v2크기만큼 v1원소를 정렬한 후 v2에 복사
 //
 #include <iostream>
 #include <vector>
@@ -48,6 +52,7 @@ public:
 };
 
 void Func_Rand(int& i) { i = rand()%1000; }
+int Func_Rand_Gen(){ return rand()%1000; }
 
 int main(void)
 {
@@ -132,4 +137,59 @@ int main(void)
     cout<<"[middle_iter, v4.end) : ";
     for(iter = middle_iter; iter != v4.end(); ++iter) cout<<*iter<<' ';
     cout<<endl<<endl;
+    
+    //quick sort
+    cout<<"3) sort (quick sort)"<<endl;
+    vector<int> v5(100);
+    generate(v5.begin(), v5.end(), Func_Rand_Gen);
+    cout<<"v5 : "; PrintV()(v5);
+    cout<<endl;
+    sort(v5.begin(), v5.end());
+    cout<<"quick sort(v5) : "; PrintV()(v5);
+    cout<<endl;
+    
+    //stable_sort (merge sort)
+    cout<<"4) stable_sort (merge sort)"<<endl;
+    vector<int> v6;
+    v6.push_back(30);
+    v6.push_back(50);
+    v6.push_back(30);
+    v6.push_back(20);
+    v6.push_back(40);
+    v6.push_back(10);
+    v6.push_back(40);
+    
+    cout<<"v6 : "; PrintV()(v6);
+    cout<<endl;
+    stable_sort(v6.begin(), v6.end(), greater<int>());
+    cout<<"stable_sort(greater) : "; PrintV()(v6);
+    cout<<endl;
+    
+    //partial_sort (heap sort)
+    cout<<"5) partial_sort (heap sort)"<<endl;
+    vector<int> v7;
+    for(int i = 0; i < 100; ++i)
+    {
+        v7.push_back(rand()%1000);
+    }
+    cout<<"v7 : "; PrintV()(v7);
+    cout<<endl;
+    partial_sort(v7.begin(), v7.begin()+20, v7.end());
+    cout<<"정렬된 상위 20개 원소 : ";
+    for(vector<int>::size_type i = 0; i < 20; ++i) cout<<v7[i]<<' ';
+    cout<<endl;
+    cout<<"정렬되지 않은 하위 80개 원소 : ";
+    for(vector<int>::size_type i = 20; i < v7.size(); ++i) cout<<v7[i]<<' ';
+    cout<<endl<<endl;
+    
+    //partial_sort_copy
+    cout<<"6) partial_sort_copy"<<endl;
+    vector<int> v8(100);
+    vector<int> v9(20);
+    generate(v8.begin(), v8.end(), Func_Rand_Gen);
+    partial_sort_copy(v8.begin(), v8.end(), v9.begin(), v9.end());
+    cout<<"v8 : "; PrintV()(v8);
+    cout<<endl;
+    cout<<"v9 : "; PrintV()(v9);
+    cout<<endl;
 }
