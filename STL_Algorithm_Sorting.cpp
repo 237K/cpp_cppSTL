b2ed219829c324dee913dff3e92f81e8351ac99e
237K <237ref@gmail.com>
2019-01-16 15:42:09 +0900

    [STL 알고리즘] <5.정렬 알고리즘> 1)heap 1-c)pop_heap, 1-d)sort_heap

--- STL_Algorithm_Sorting.cpp
+++ STL_Algorithm_Sorting.cpp
@@ -7,8 +7,10 @@
 //  STL 알고리즘 #include <algorithm>
 //      5. 정렬 알고리즘
 //          1) heap
-//              a) make_heap(b, e) : 벡터와 힙구조를 항상 상상하자 벡터의 각 원소가 힙의 어디에 위치하는지
-//              b) push_heap(b, e) : vector에 push_back 한 다음 사용
+//              a) make_heap(b, e, (f)) : 벡터와 힙구조를 항상 상상하자 벡터의 각 원소가 힙의 어디에 위치하는지
+//              b) push_heap(b, e, (f)) : vector에 push_back 한 다음 사용
+//              c) pop_heap(b, e, (f)) : 루트노드(가장 큰 원소) 제거. 루트노드와 마지막노드를 교환한 후 힙정렬 실행됨
+//              d) sort_heap(b, e, (f)) : 힙정렬
 //
 #include <iostream>
 #include <vector>
@@ -67,4 +69,46 @@ int main(void)
     push_heap(v1.begin(), v1.end());
     cout<<"v1(heap) : "; PrintV()(v1);
     cout<<endl;
+    
+    //pop_heap
+    cout<<"1-c) pop_heap"<<endl;
+    cout<<"v1(heap) : "; PrintV()(v1);
+    cout<<"pop_heap!"<<endl;
+    pop_heap(v1.begin(), v1.end());
+    cout<<"v1(heap) : "; PrintV()(v1);
+    cout<<endl;
+    
+    //sort_heap
+    cout<<"1-d) sort_heap"<<endl;
+    vector<int> v2(6);
+    vector<int> v3(6);
+    generate(v2.begin(), v2.end(), Integer10<int>());
+    generate(v3.begin(), v3.end(), Integer10<int>());
+    cout<<"v2 : "; PrintV()(v2);
+    cout<<"v3 : "; PrintV()(v3);
+    cout<<endl;
+    
+    make_heap(v2.begin(), v2.end());
+    make_heap(v3.begin(), v3.end(), greater<int>());
+    cout<<"make_heap(default) : "; PrintV()(v2);
+    cout<<"make_heap(greater) : "; PrintV()(v3);
+    cout<<endl;
+    
+    v2.push_back(35);
+    v3.push_back(35);
+    push_heap(v2.begin(), v2.end());
+    push_heap(v3.begin(), v3.end(), greater<int>());
+    cout<<"push_heap(default) : "; PrintV()(v2);
+    cout<<"push_heap(greater) : "; PrintV()(v3);
+    cout<<endl;
+    
+    sort_heap(v2.begin(), v2.end());
+    sort_heap(v3.begin(), v3.end(), greater<int>());
+    cout<<"sort_heap(default) : "; PrintV()(v2);
+    cout<<"sort_heap(greater) : "; PrintV()(v3);
+    cout<<endl;
+    sort_heap(v2.begin(), v2.end(), greater<int>());
+    cout<<"make랑 push를 디폴트로 한 경우에 sort를 greater로 하면 : "; PrintV()(v2);
+    cout<<endl;
+    
 }
