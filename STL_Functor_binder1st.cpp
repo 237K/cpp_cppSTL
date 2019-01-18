1d3ad69241d10dbc6085ec7624e8140c698f1ce5
237K <237ref@gmail.com>
2019-01-18 18:46:36 +0900

    [STL Functor] 1)펑터에 타입을 모두 정의하는 방법으로 이항연산자를 단항연산자처럼 기능하도록 만들어보았습니다.

---
+++ STL_Functor_binder1st.cpp
@@ -0,0 +1,71 @@
+//
+//  STL_Functor.cpp
+//  cpp_study
+//
+//  Created by 형성모 on 18/01/2019.
+//
+//  STL Functor
+//  binder1st<int>를 사용하여 binary_function을 unary_function처럼 기능하도록 해보았습니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+class Printv
+{
+public:
+    void operator()(const vector<int>& v)
+    {
+        for(vector<int>::size_type i = 0; i < v.size(); ++i) cout<<v[i]<<' ';
+        cout<<endl;
+    }
+};
+
+template <typename T>
+class Plus
+{
+public:
+    typedef T first_argument_type;
+    typedef T second_argument_type;
+    typedef T result_type;
+public:
+    T operator()(const T& left, const T& right) const { return left+right; }
+};
+
+int main(void)
+{
+    vector<int> v1;
+    vector<int> v2;
+    vector<int> v3(5);
+    vector<int> v4(5);
+    
+    v1.push_back(10);
+    v1.push_back(20);
+    v1.push_back(30);
+    
+    v2.push_back(1);
+    v2.push_back(2);
+    v2.push_back(3);
+    
+    cout<<"v1 : "; Printv()(v1);
+    cout<<"v2 : "; Printv()(v2);
+    transform(v1.begin(), v1.end(), v2.begin(), v3.begin(), Plus<int>());
+    cout<<"v3(user's Plus) : "; Printv()(v3);
+    transform(v1.begin(), v1.end(), v2.begin(), v4.begin(), plus<int>());
+    cout<<"v4(STL plus) : "; Printv()(v4);
+    cout<<endl;
+    
+    //binder1st<int>를 사용하여 한 항을 100으로 고정
+    cout<<"binder1st<int>를 사용하여 transform을 단항연산처럼 기능하도록 해보았습니다."<<endl;
+    transform(v1.begin(), v1.end(), v3.begin(), binder1st<Plus<int> > (Plus<int>(), 100));
+    cout<<"v1 : "; Printv()(v1);
+    cout<<"v3(user's Plus) : "; Printv()(v3);
+    cout<<endl;
+    transform(v1.begin(), v1.end(), v4.begin(), binder1st<plus<int> > (plus<int>(), 100));
+    cout<<"v1 : "; Printv()(v1);
+    cout<<"v4(STL plus) : "; Printv()(v4);
+    
+}
